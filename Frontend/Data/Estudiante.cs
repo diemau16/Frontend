4367d53e03ceb353006ff6cf23db4966bc378dbf
diemau16 <diemauacro@gmail.com>
2022-05-28 23:29:57 -0500

    Agregar archivos de proyecto.

---
+++ Frontend/Data/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.Data
+{
+    public class Estudiante
+    {
+        [Key]
+        public int idEstudiante { get; set; }
+        public string nombre { get; set; }
+        public string carrera { get; set; }
+        public int semestre { get; set; }
+        public string correo { get; set; }
+    }
+}
---
+++ Frontend/Data/Mascota.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.Data
+{
+    public class Mascota
+    {
+        [Key]
+        public int celularDueño { get; set; }
+        public string nombre { get; set; }
+        public string raza { get; set; }
+        public int edad { get; set; }
+        public string nombreDueño { get; set; }
+    }
+}
